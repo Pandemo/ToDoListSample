cb9e66cdc19cce54b9e4d94007e90c8a6cc63637
Sahejvir Locham <Sahej@sahejvirs-macbook-pro.local>
2015-10-24 13:40:56 -0400

    First Commit with Base Code + Persistent Storage Code for Swift2

--- To-Do-List-First/FirstViewController.swift
+++ To-Do-List-First/FirstViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+//create global array of strngs
+var toDoListArr = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //check if this exists for the first time app runs
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoListArr") != nil {
+            
+               toDoListArr = NSUserDefaults.standardUserDefaults().objectForKey("toDoListArr") as! [String]
+            
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,34 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return toDoListArr.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoListArr[indexPath.row]
+        return cell
+    }
+    //swipe to delete feature
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+    
+        if editingStyle == UITableViewCellEditingStyle.Delete{
+            
+            toDoListArr.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject("toDoListArrat", forKey: "toDoListArray")
+            table.reloadData()
+        }
+        
+        
+    }
+    
+    //runs everytime the view appears, not only when its loaded.
+    override func viewDidAppear(animated: Bool) {
+        table.reloadData()
+    }
 }
 
--- To-Do-List-First/SecondViewController.swift
+++ To-Do-List-First/SecondViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var textItem: UITextField!
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoListArr.append(textItem.text!)
+        textItem.text = ""
+        NSUserDefaults.standardUserDefaults().setObject(toDoListArr, forKey: "toDoListArr")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,17 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //keyboard functions to retract when touched outside/return pressed
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn (textField: UITextField!) -> Bool{
+        
+        textItem.resignFirstResponder()
+        return true
+    }
 
 }
 
